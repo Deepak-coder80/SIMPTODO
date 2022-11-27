d56035204a2cdbcbdb71b91802e67ba475510894
Deepak M S <deepakcoder80@gmail.com>
2022-11-27 12:17:13 +0530

    home screen basic design finished

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Home Screen'),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.blueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child:const Icon(Icons.add,size: 30,),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 40, top: 40),
+            child: const Text(
+              'TODOs',
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: Container(
+              decoration:const  BoxDecoration(
+               color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25)
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
